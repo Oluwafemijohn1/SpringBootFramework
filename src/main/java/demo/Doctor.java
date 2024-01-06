df73f29f64e21453bee5732db5364dcc62352bb6
Oluwafemijohn1 <femiogundipe01@gmail.com>
2024-01-06 21:50:09 +0100

    Injecting an object in a field.

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -2,7 +2,26 @@ package demo;
 
 public class Doctor implements Staff {
 
+    private String qualification;
+    private Nurse nurse;
+
     public void assist(){
-        System.out.println("Doctor assigning ");
+        System.out.println("Doctor assigning with qualification of " + this.qualification);
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public Nurse getNurse() {
+        return nurse;
+    }
+
+    public void setNurse(Nurse nurse) {
+        this.nurse = nurse;
     }
 }
