f0837e0ec8b19485b0fd982cf078c0610929a9fb
Oluwafemijohn1 <femiogundipe01@gmail.com>
2024-01-07 22:51:11 +0100

    Life cycle scope

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -2,7 +2,6 @@ package demo;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/demo/Nurse.java
+++ src/main/java/demo/Nurse.java
@@ -1,7 +1,5 @@
 package demo;
 
-import org.springframework.stereotype.Component;
-
 public class Nurse implements Staff {
     public void assist(){
         System.out.println("Nurse assisting ");
